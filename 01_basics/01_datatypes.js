ef6e714531d8a3f1a1ebf4d4642518957758cbe3
Pranjal Jaiswal <155170517+pranjal501@users.noreply.github.com>
2024-11-26 07:27:00 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"  //treat all js code as newer version of js
+
+// alert(3+3)  //error because we are using nodejs not browser
+
+let name="pranjal"
+let age=19
+let isLoggedIn=false
+
+
+//DATATYPES - 
+// number => range 2 to the power 53
+//bigint
+//string => ""
+//boolean => true/false
