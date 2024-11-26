9d5cd58c8bc21cc4a36ad5ff611bcd6b07547983
Pranjal Jaiswal <155170517+pranjal501@users.noreply.github.com>
2024-11-26 06:15:12 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=1234
+let accountemail="pranjaljaiswal@gmail.com"
+var accountpassword="pj00000@11"
+accountCity = "Jaipur"
+//accountId=2  //not allowed
+
+accountemail="pj@email.com"
+accountpassword="pj11"
+accountCity="Kota"
+
+console.log(accountId);
+console.table(accountId,accountemail,accountpassword,accountCity);
